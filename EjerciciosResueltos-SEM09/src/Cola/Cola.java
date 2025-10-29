d9d79cc2181b492e3910ecc5604cebff99e44b1d
JTR-14 <djtoledor@unitru.edu.pe>
2025-10-28 19:55:26 -0500

    ini

---
+++ EjerciciosResueltos-SEM09/src/Cola/Cola.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Cola;
+
+public class Cola<T> {
+
+    private Nodo<T> primero;
+    private Nodo<T> ultimo;
+
+    public Cola() {
+        primero = null;
+        ultimo = null;
+    }
+
+    public void encolar(T valor) {
+        Nodo<T> nuevo = new Nodo<>(valor);
+        if (primero == null) {
+            primero = nuevo;
+            ultimo = nuevo;
+        } else {
+            ultimo.setSgte(nuevo);
+        }
+        ultimo = nuevo;
+    }
+
+    public T desencolar() {
+        T eliminado = primero.getInfo();
+        primero = primero.getSgte();
+        if (primero == null) {
+            ultimo = null;
+        }
+        return eliminado;
+    }
+
+    public boolean esVacio() {
+        return primero == null;
+    }
+
+    public T cima() {
+        return primero.getInfo();
+    }
+
+    public void eliminarTodos() {
+        while (primero != null) {
+            primero = primero.getSgte();
+        }
+        ultimo = null;
+    }
+}
